a4df948fc38b437f4ee94d88cb9a30d7b5b55d26
Anastsiia <nasya090099@gmail.com>
2022-11-09 23:51:44 +0200

    Lab5 (3 activity and viewBinding)

--- app/src/main/java/com/example/applicationwith3activity/MainActivity.kt
+++ app/src/main/java/com/example/applicationwith3activity/MainActivity.kt
@@ -1,21 +1,45 @@
 package com.example.applicationwith3activity
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
+import android.widget.Toast
+import com.example.applicationwith3activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val text = findViewById<View>(R.id.textView2) as TextView
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val amimator: Animation = AnimationUtils.loadAnimation(this, R.anim.first_anim)
-        text.startAnimation(amimator)
+        binding.textView2.startAnimation(amimator)
+
+        binding.button.setOnClickListener(::buttonClick)
+
+        /*
+        не уверенна, что оно корректно сработает
+        хочется, чтобы при долгом нажатии на HI! повторилась анимация
+        */
+        binding.textView2.setOnLongClickListener (object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                binding.textView2.startAnimation(amimator)
+                return true
+            }
+        })
+    }
+
+    private fun buttonClick(v: View) {
+        Toast.makeText(this@MainActivity,"Thank you for click\n on new lab you can go to new activity♥", Toast.LENGTH_LONG) .show()
     }
 }
